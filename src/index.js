244fe87177b8fc0380809d260651651cde5252ab
Stephanie Bertram <steph.a.bertram@gmail.com>
2024-02-01 16:22:01 -0800

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form')
+  const description = document.querySelector('#new-task-description')
+  const ul = document.querySelector('#tasks')
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    const task = description.value;
+    const li = document.createElement('li')
+    li.innerText = task
+    ul.appendChild(li)
+    const button = document.createElement('button')
+    button.innerText = 'x'
+    button.addEventListener('click', (e) => li.remove())
+    li.appendChild(button)
+    form.reset()
+  }
+ 
+  form.addEventListener('submit', handleSubmit)
 });
